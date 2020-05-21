147726574252c16ff6f8906d1a9c08476ca63a71
Esther Kang <estherkang14@gmail.com>
2020-05-21 18:50:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry' 
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  final_results = data.each_with_object({}) do | (key, value), final_hash|
+    #binding.pry 
+    value.each do |inner_key, names|
+      #binding.pry 
+      names.each do |name|
+        #binding.pry 
+        if !final_hash[name]
+          final_hash[name] = {}
+          #binding.pry 
+        end
+        if !final_hash[name][key]
+          final_hash[name][key] = []
+          #binding.pry 
+        end
+        final_hash[name][key].push(inner_key.to_s)
+      end 
+    end 
+   
+  end 
+  #binding.pry 
 end
